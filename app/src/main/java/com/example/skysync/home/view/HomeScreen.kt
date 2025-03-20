e10c8615c75101fcae5c2955d67e92e6e7ce9638
Hany Samy <hs1547@fayoum.edu.eg>
2025-03-21 01:06:23 +0200

    set currentTemp and city Weather Des with Static Data

--- app/src/main/java/com/example/skysync/home/view/HomeScreen.kt
+++ app/src/main/java/com/example/skysync/home/view/HomeScreen.kt
@@ -1,35 +1,73 @@
 package com.example.skysync.home.view
 
-import android.util.Log
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import com.example.skysync.home.viewmodel.CurrentWeatherViewModelImp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 private const val TAG = "HomeScreen"
-@Composable
-fun HomeScreen(navController: NavController, viewModel: CurrentWeatherViewModelImp) {
 
-    viewModel.getCurrentWeather(29.394859, 30.9028837, "en")
+@Preview()
+@Composable
+fun HomeScreen(/*navController: NavController, viewModel: CurrentWeatherViewModelImp*/) {
+    val horizontalScroll = rememberScrollState()
+    val date = "20,mar"
+    val time = "10:23 PM"
+    val feelsLike = "15"
+    val currentTemperature = -5
+    val tempMeasure = "C"
+    /*viewModel.getCurrentWeather(29.394859, 30.9028837, "en")
     val weatherState = viewModel.weather.observeAsState()
     val messageState = viewModel.message.observeAsState()
-val currentWeather = weatherState.value
+    val currentWeather = weatherState.value
 
-    Log.i(TAG, "HomeScreen: ${currentWeather?.weather?.get(0)?.description}")
-    Box(
+    Log.i(TAG, "HomeScreen: ${currentWeather?.weather?.get(0)?.description}")*/
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .verticalScroll(horizontalScroll)
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        Text(
-            text = "Home Screen, ${currentWeather?.weather?.get(0)?.description}",
-            style = MaterialTheme.typography.headlineLarge
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            //.height(60.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Fayoum", fontSize = 25.sp)
+                Text("$date  $time", fontSize = 20.sp)
+            }
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Clear", fontSize = 20.sp)
+                Text("Feels $feelsLike", fontSize = 20.sp)
+            }
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.Top) {
+             Text("$currentTemperature", fontSize = 120.sp)
+             Text(tempMeasure, fontSize = 20.sp , modifier = Modifier.padding(top=12.dp))
+        }
+        /*Text(
+                   text = "Home Screen, ${currentWeather?.weather?.get(0)?.description}",
+                   style = MaterialTheme.typography.headlineLarge
+               )*/
     }
 }
