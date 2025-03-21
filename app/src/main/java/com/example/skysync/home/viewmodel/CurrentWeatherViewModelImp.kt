269f194ba89b9b5726a3c02483aa2cb3cdfa0ebd
Hany Samy <hs1547@fayoum.edu.eg>
2025-03-21 23:50:58 +0200

    Home Ui V1 and real Data

--- app/src/main/java/com/example/skysync/home/viewmodel/CurrentWeatherViewModelImp.kt
+++ app/src/main/java/com/example/skysync/home/viewmodel/CurrentWeatherViewModelImp.kt
@@ -1,31 +1,60 @@
 package com.example.skysync.home.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.skysync.models.CurrentWeatherResponse
+import com.example.skysync.models.ForecastWeatherResponse
 import com.example.skysync.repo.WeatherRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+private const val TAG = "CurrentWeatherViewModel"
 class CurrentWeatherViewModelImp(private val repo: WeatherRepository) : CurrentWeatherViewModel,
     ViewModel() {
     private val mutableWeather: MutableLiveData<CurrentWeatherResponse> = MutableLiveData()
     val weather: LiveData<CurrentWeatherResponse> = mutableWeather
 
+    private val mutableForecast: MutableLiveData<ForecastWeatherResponse> = MutableLiveData()
+    val forecast: LiveData<ForecastWeatherResponse> = mutableForecast
+
     private val mutableMessage: MutableLiveData<String> = MutableLiveData()
     val message: LiveData<String> = mutableMessage
     override  fun getCurrentWeather(
-        lat: Double, lon: Double, lang: String
+        lat: Double, lon: Double, lang: String, unit : String
     ) {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = null//repo.getCurrentWeather(lat, lon, lang)
+                val result = repo.getCurrentWeather(lat, lon, lang,unit)
                 if (result != null) {
                     val weather: CurrentWeatherResponse = result
                     mutableWeather.postValue(weather)
+                    Log.i(TAG, "getCurrentWeather: $weather")
+                } else {
+                    mutableMessage.postValue("Tray Again Later")
+                }
+            } catch (e: Exception) {
+                mutableMessage.postValue("Error ${e.message}")
+            }
+        }
+    }
+
+    override fun getForecast(
+        lat: Double,
+        lon: Double,
+        lang: String,
+        unit: String
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = repo.getForecast(lat, lon, lang,unit)
+                if (result != null) {
+                    val forecast: ForecastWeatherResponse = result
+                    mutableForecast.postValue(forecast)
+                    Log.i(TAG, "getCurrentWeather: $weather")
                 } else {
                     mutableMessage.postValue("Tray Again Later")
                 }
